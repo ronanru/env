f927439b4b88ff925d977e70b43268bea421a67c
Matvey <35634442+ronanru@users.noreply.github.com>
2025-07-21 15:56:53 -0300

    bump

--- install.sh
+++ install.sh
@@ -9,7 +9,9 @@ curl -fsSL "https://copr.fedorainfracloud.org/coprs/scottames/ghostty/repo/fedor
 rpm-ostree refresh-md
 rpm-ostree install ghostty
 
-brew install tmux neovim gh starship ripgrep eza bat tailscale jq node fastfetch ffmpeg yt-dlp tokei fish btop fzf lazygit difftastic tlrc static-web-server oven-sh/bun/bun sst/tap/opencode
+brew install gh ripgrep eza bat node ffmpeg yt-dlp tokei btop lazygit difftastic tlrc static-web-server sst/tap/opencode pnpm
+
+curl -fsSL https://bun.sh/install | bash
 
 flatpak install -y --noninteractive flathub com.obsproject.Studio org.chromium.Chromium org.libreoffice.LibreOffice io.mpv.Mpv com.google.AndroidStudio org.qbittorrent.qBittorrent org.signal.Signal com.system76.Popsicle com.valvesoftware.Steam
 
@@ -45,6 +47,7 @@ kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 7"
 kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 8" "Meta+*,Meta+*,Window to Desktop 8"
 kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 9" "Meta+(,Meta+(,Window to Desktop 9"
 
+kwriteconfig6 --file kglobalshortcutsrc --group plasmashell --key "manage activities" "none,,Show Activity Switcher"
 kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Window Close" "Meta+Q,Meta+Q,Close Window"
 
 kwriteconfig6 --file kglobalshortcutsrc --group services --group com.mitchellh.ghostty.desktop --key "_launch" "Meta+Return"
@@ -56,7 +59,7 @@ kwriteconfig6 --file kglobalshortcutsrc --group org_kde_powerdevil --key "powerP
 kwriteconfig6 --file kglobalshortcutsrc --group services --group org.chromium.Chromium.desktop --key "_launch" "Meta+B"
 
 kwriteconfig6 --file kglobalshortcutsrc --group plasmashell --key "activate application launcher" "none,,Activate Application Launcher"
-kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Overview" "Meta+W\tMeta,Meta+W,Toggle Overview"
+kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Overview" "Meta,Meta+W,Toggle Overview"
 
 kwriteconfig6 --file kwinrc --group Plugins --key "slideEnabled" "false"
 kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key "ButtonsOnLeft" ""
@@ -71,7 +74,6 @@ kwriteconfig6 --file kxkbrc --group Layout --key "Use" "true"
 kwriteconfig6 --file kxkbrc --group Layout --key "VariantList" ","
 
 kwriteconfig6 --file kdeglobals --group "KDE" --key "AnimationDurationFacto" "0"
-kwriteconfig6 --file kdeglobals --group "General" --key "ColorScheme" "BreezeDark"
 kwriteconfig6 --file kdeglobals --group "General" --key "TerminalApplication" "ghostty"
 kwriteconfig6 --file kdeglobals --group "General" --key "TerminalService" "com.mitchellh.ghostty.desktop"
 
@@ -84,9 +86,11 @@ cp ./config/ghostty ~/.config/ghostty/config
 rm -rf ~/.config/tmux/plugins/catppuccin
 mkdir -p ~/.config/tmux/plugins/catppuccin
 git clone --depth 1 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
-cp ./config/tmux.conf ~/.config/tmux
+cp ./config/tmux.conf ~/.tmux.conf
 
 mkdir -p ~/.config/fish/themes
+rm ~/.config/starship.toml
+starship preset nerd-font-symbols -o ~/.config/starship.toml
 wget "https://raw.githubusercontent.com/catppuccin/fish/refs/heads/main/themes/Catppuccin%20Mocha.theme" -O ~/.config/fish/themes/Catppuccin\ Mocha.theme
 cp ./config/config.fish ~/.config/fish
 fish -c "yes | fish_config theme save \"Catppuccin Mocha\""
@@ -112,4 +116,4 @@ wget "https://raw.githubusercontent.com/ThePrimeagen/tmux-sessionizer/refs/heads
 cp ./config/tmux-sessionizer.conf ~/.config/tmux-sessionizer
 chmod +x ~/.local/bin/tmux-sessionizer
 
-bun i -g @antfu/ni taze
+~/.bun/bin/bun i -g @antfu/ni taze
