dcaffba29d75a1c2ab89b117913151677de0cccf
matvey <35634442+matrv@users.noreply.github.com>
2026-07-09 13:20:09 +0200

    bump

--- config/config.fish
+++ config/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
     fish_add_path ~/Android/Sdk/platform-tools
     fish_add_path ~/Android/Sdk/build-tools
     fish_add_path ~/.bun/bin
+    fish_add_path ~/.vite-plus/bin
 
     /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
 
